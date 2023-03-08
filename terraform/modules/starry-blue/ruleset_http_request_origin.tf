b9c55a797fb38cf4bf1e41d3f5c317db785b0c56
SlashNephy <spica@starry.blue>
2023-03-08 23:36:32 +0900

    オリジン追加漏れ

--- terraform/modules/starry-blue/ruleset_http_request_origin.tf
+++ terraform/modules/starry-blue/ruleset_http_request_origin.tf
@@ -22,6 +22,7 @@ resource "cloudflare_ruleset" "http_request_origin" {
         "${cloudflare_record.cname_asf.hostname}"
         "${cloudflare_record.cname_atmos.hostname}"
         "${cloudflare_record.cname_basic.hostname}"
+        "${cloudflare_record.cname_code.hostname}"
         "${cloudflare_record.cname_epgstation.hostname}"
         "${cloudflare_record.cname_files.hostname}"
         "${cloudflare_record.cname_jupyter.hostname}"
