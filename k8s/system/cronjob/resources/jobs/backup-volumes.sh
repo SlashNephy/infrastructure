0554a1156305d2e149f2644c6e1ec8bb0e9dd174
SlashNephy <spica@starry.blue>
2023-03-11 18:36:02 +0900

    スクリプトを埋め込む

--- k8s/system/cronjob/resources/jobs/backup-volumes.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ash
-
-set -e
-
-apk add zstd
-tar -acpf /dist/volumes.zst /src || true
-rclone copy /dist horoscope:Backup --config /rclone.conf
-rm -f /dist/volumes.zst
--- k8s/system/cronjob/resources/jobs/create-tvtest-release.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ash
-
-apk add github-cli
-gh api \
-  --method POST \
-  -H "Accept: application/vnd.github.v3+json" \
-  /repos/SlashNephy/TVTest-builder/releases \
-  -f tag_name="$(date -Idate)"
