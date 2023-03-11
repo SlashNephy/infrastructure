69fc0aaf1c70a4a275fb8eca142074341505d8cc
SlashNephy <spica@starry.blue>
2023-03-11 19:56:31 +0900

    direct.starry.blue を再追加

--- terraform/modules/starry-blue/record_cname.tf
+++ terraform/modules/starry-blue/record_cname.tf
@@ -287,3 +287,12 @@ resource "cloudflare_record" "cname_anemos" {
   type    = "CNAME"
   proxied = true
 }
+
+// 互換性のために残す
+resource "cloudflare_record" "cname_direct" {
+  zone_id = cloudflare_zone.zone.id
+  name    = "direct"
+  value   = cloudflare_record.a_gateway.hostname
+  type    = "CNAME"
+  proxied = false
+}
