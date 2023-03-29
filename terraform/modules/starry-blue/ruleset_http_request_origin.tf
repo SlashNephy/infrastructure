6773226bfadfc9bdd03b0c8c1f8ec51f14ed049e
SlashNephy <spica@starry.blue>
2023-03-29 13:06:48 +0900

    オリジン追加もれ

--- terraform/modules/starry-blue/ruleset_http_request_origin.tf
+++ terraform/modules/starry-blue/ruleset_http_request_origin.tf
@@ -29,6 +29,7 @@ resource "cloudflare_ruleset" "http_request_origin" {
         "${cloudflare_record.cname_k8s.hostname}"
         "${cloudflare_record.cname_keel.hostname}"
         "${cloudflare_record.cname_konomitv.hostname}"
+        "${cloudflare_record.cname_mahiron.hostname}"
         "${cloudflare_record.cname_mirakurun.hostname}"
         "${cloudflare_record.cname_owncast.hostname}"
         "${cloudflare_record.cname_rclone.hostname}"
