8d4e72e6eeb584c42ffe12df93da54191d46846d
SlashNephy <spica@starry.blue>
2023-04-10 13:01:00 +0900

    cattleya.starry.blue

--- terraform/modules/starry-blue/record_a.tf
+++ terraform/modules/starry-blue/record_a.tf
@@ -13,3 +13,11 @@ resource "cloudflare_record" "a_lily" {
   type    = "A"
   proxied = false
 }
+
+resource "cloudflare_record" "a_cattleya" {
+  zone_id = cloudflare_zone.zone.id
+  name    = "cattleya"
+  value   = "192.168.0.3"
+  type    = "A"
+  proxied = false
+}
