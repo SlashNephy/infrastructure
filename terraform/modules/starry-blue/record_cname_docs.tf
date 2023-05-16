3eaf1528699ea18113ad8666c52ae63dcff18af1
SlashNephy <spica@starry.blue>
2023-05-16 17:10:41 +0900

    Cloudflare Pages を管理しない

--- terraform/modules/starry-blue/record_cname_docs.tf
+++
@@ -1,7 +0,0 @@
-resource "cloudflare_record" "cname_docs" {
-  zone_id = cloudflare_zone.zone.id
-  name    = "docs"
-  value   = "starrybluesky-documents.pages.dev"
-  type    = "CNAME"
-  proxied = true
-}
--- terraform/modules/starry-blue/record_cname_xiv.tf
+++
@@ -1,7 +0,0 @@
-resource "cloudflare_record" "cname_xiv" {
-  zone_id = cloudflare_zone.zone.id
-  name    = "xiv"
-  value   = "divination.pages.dev"
-  type    = "CNAME"
-  proxied = true
-}
