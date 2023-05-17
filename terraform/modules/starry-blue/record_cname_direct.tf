3ed1ccaf5a0da65f5cd9a50b3e3e71904d6588f4
SlashNephy <spica@starry.blue>
2023-05-18 00:59:35 +0900

    不要な CNAME も削除

--- terraform/modules/starry-blue/record_cname_direct.tf
+++
@@ -1,8 +0,0 @@
-// 互換性のために残す
-resource "cloudflare_record" "cname_direct" {
-  zone_id = cloudflare_zone.zone.id
-  name    = "direct"
-  value   = cloudflare_record.a_gateway.hostname
-  type    = "CNAME"
-  proxied = false
-}
