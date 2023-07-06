34efdd5ed4800b06e7ea256fb69c11e08074134c
SlashNephy <spica@starry.blue>
2023-07-06 17:15:50 +0900

    _atproto.starry.blue TXT

--- terraform/modules/starry-blue/cloudflare_record_txt_bluesky.tf
+++ terraform/modules/starry-blue/cloudflare_record_txt_bluesky.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "txt_bluesky" {
   zone_id = cloudflare_zone.zone.id
-  name    = "_atproto.spica"
+  name    = "_atproto"
   value   = "did=did:plc:blaqv5fitmt5jbhfqjizh6ua"
   type    = "TXT"
 }
