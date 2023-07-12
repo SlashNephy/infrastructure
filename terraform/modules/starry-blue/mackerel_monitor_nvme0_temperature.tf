b3f91ea4cc99e79e455c924be8ce5657873b5994
SlashNephy <spica@starry.blue>
2023-07-12 14:53:36 +0900

    add temperature monitor

---
+++ terraform/modules/starry-blue/mackerel_monitor_nvme0_temperature.tf
@@ -0,0 +1,13 @@
+resource "mackerel_monitor" "cpu_temperature" {
+  name = "ストレージ温度が高すぎる (nvme0)"
+
+  host_metric {
+    metric             = "custom.telegraf.smart_attribute.device-nvme0_host-lily_model-Samsung_SSD_970_EVO_250GB_name-Temperature_Sensor_1_serial_no-S465NB0K407096Z.raw_value"
+    operator           = ">"
+    warning            = "50"
+    critical           = "60"
+    duration           = 3
+    max_check_attempts = 1
+    scopes             = [mackerel_role.server.id]
+  }
+}
