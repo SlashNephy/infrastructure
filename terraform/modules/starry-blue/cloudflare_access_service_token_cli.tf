71f20da8dbc385dd1144f085bc25bfa061c156b0
SlashNephy <spica@starry.blue>
2023-09-10 22:39:35 +0900

    restore "cloudflare_access_service_token" "cli"

---
+++ terraform/modules/starry-blue/cloudflare_access_service_token_cli.tf
@@ -0,0 +1,9 @@
+resource "cloudflare_access_service_token" "cli" {
+  account_id           = cloudflare_account.account.id
+  name                 = "CLI"
+  min_days_for_renewal = 30
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
