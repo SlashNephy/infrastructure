616ad029822a39d5d30ef3db1768f15b19bdef2f
SlashNephy <spica@starry.blue>
2023-09-12 13:56:03 +0900

    DMARC を CF 管理にする

--- terraform/modules/starry-blue/cloudflare_record_txt_dmarc.tf
+++
@@ -1,6 +0,0 @@
-resource "cloudflare_record" "txt_dmarc" {
-  zone_id = cloudflare_zone.zone.id
-  name    = "_dmarc"
-  value   = "v=DMARC1; p=quarantine; rua=mailto:${local.webmaster_email_address}"
-  type    = "TXT"
-}
