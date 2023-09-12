1ec211c6e4ddf38755935fcae56845d819ee7ce8
SlashNephy <spica@starry.blue>
2023-09-12 15:52:26 +0900

    mirakurun を廃止する

--- terraform/modules/starry-blue/cloudflare_record_cname_mirakurun.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname_mirakurun.tf
@@ -1,35 +1,8 @@
+// Deprecated: mahiron.starry.blue にリダイレクトされる
 resource "cloudflare_record" "cname_mirakurun" {
   zone_id = cloudflare_zone.zone.id
   name    = "mirakurun"
   value   = cloudflare_record.a_gateway.hostname
   type    = "CNAME"
   proxied = true
-}
-
-resource "mackerel_service" "mirakurun" {
-  name = "Lily_Mirakurun"
-}
-
-resource "mackerel_monitor" "mirakurun" {
-  name = format("%s に疎通できない", cloudflare_record.cname_mirakurun.hostname)
-
-  external {
-    method                 = "GET"
-    url                    = format("https://%s", cloudflare_record.cname_mirakurun.hostname)
-    service                = mackerel_service.mirakurun.name
-    response_time_warning  = 500
-    response_time_critical = 1000
-    response_time_duration = 3
-    max_check_attempts     = 3
-    headers                = {
-      Cache-Control           = "no-cache"
-      CF-Access-Client-Id     = data.onepassword_item.cloudflare_access_client.username
-      CF-Access-Client-Secret = data.onepassword_item.cloudflare_access_client.password
-    }
-    certification_expiration_warning  = 14
-    certification_expiration_critical = 7
-    follow_redirect                   = false
-  }
-
-  depends_on = [data.onepassword_item.cloudflare_access_client]
 }
