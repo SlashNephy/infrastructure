badc68ab9e98de48828f2cf2a6e8156876e49791
SlashNephy <spica@starry.blue>
2023-09-26 02:07:20 +0900

    ai.starry.blue の Mackerel を取る

--- terraform/modules/starry-blue/cloudflare_record_cname_ai.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname_ai.tf
@@ -5,31 +5,3 @@ resource "cloudflare_record" "cname_ai" {
   type    = "CNAME"
   proxied = true
 }
-
-resource "mackerel_service" "ai" {
-  name = "Lily_Stable-Diffusion-WebUI"
-}
-
-resource "mackerel_monitor" "ai" {
-  name = format("%s に疎通できない", cloudflare_record.cname_ai.hostname)
-
-  external {
-    method                 = "GET"
-    url                    = format("https://%s", cloudflare_record.cname_ai.hostname)
-    service                = mackerel_service.ai.name
-    response_time_warning  = 3000
-    response_time_critical = 5000
-    response_time_duration = 3
-    max_check_attempts     = 1
-    headers                = {
-      Cache-Control           = "no-cache"
-      CF-Access-Client-Id     = data.onepassword_item.cloudflare_access_client.username
-      CF-Access-Client-Secret = data.onepassword_item.cloudflare_access_client.password
-    }
-    certification_expiration_warning  = 14
-    certification_expiration_critical = 7
-    follow_redirect                   = false
-  }
-
-  depends_on = [data.onepassword_item.cloudflare_access_client]
-}
