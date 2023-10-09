86f5d4fb93575474f83965560c2187ed00bde515
SlashNephy <spica@starry.blue>
2023-10-09 15:33:15 +0900

    ダウンタイムを延ばす

--- terraform/modules/starry-blue/mackerel_downtime_weekly.tf
+++ terraform/modules/starry-blue/mackerel_downtime_weekly.tf
@@ -1,28 +1,11 @@
 resource "mackerel_downtime" "weekly" {
   name     = "定期再起動"
-  start    = 1688324400
-  duration = 360
+  start    = 1696784400
+  duration = 720
 
   recurrence {
     interval = 1
     type     = "weekly"
     weekdays = ["Monday"]
   }
-
-  monitor_scopes = [
-    mackerel_monitor.connectivity.id,
-    mackerel_monitor.mirakurun_api.id,
-    mackerel_monitor.mirakurun_api_v6.id,
-    mackerel_monitor.mahiron.id,
-    mackerel_monitor.mahiron_api.id,
-    mackerel_monitor.mahiron_api_v6.id,
-    mackerel_monitor.epgstation.id,
-    mackerel_monitor.epgstation_api.id,
-    mackerel_monitor.epgstation_api_v6.id,
-    mackerel_monitor.bbc_basic.id,
-    mackerel_monitor.bbc_k8s.id,
-    mackerel_monitor.bbc_mahiron.id,
-    mackerel_monitor.bbc_mahiron_api.id,
-    mackerel_monitor.bbc_ssh.id
-  ]
 }
