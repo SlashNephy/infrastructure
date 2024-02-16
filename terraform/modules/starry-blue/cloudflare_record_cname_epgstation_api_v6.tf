2511329c31cb2a499da69b97c4a48f0d3f5dab5d
SlashNephy <spica@starry.blue>
2024-02-16 23:21:45 +0900

    IPv6 の監視ルールを削除

--- terraform/modules/starry-blue/cloudflare_record_cname_epgstation_api_v6.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname_epgstation_api_v6.tf
@@ -5,31 +5,3 @@ resource "cloudflare_record" "cname_epgstation_api_v6" {
   type    = "CNAME"
   proxied = false
 }
-
-resource "mackerel_service" "epgstation_api_v6" {
-  name = "Lily_EPGStation-API-IPv6"
-}
-
-resource "mackerel_monitor" "epgstation_api_v6" {
-  name = format("%s に疎通できない", cloudflare_record.cname_epgstation_api_v6.hostname)
-
-  external {
-    method                 = "GET"
-    url                    = format("https://%s/api/version", cloudflare_record.cname_epgstation_api_v6.hostname)
-    service                = mackerel_service.epgstation_api_v6.name
-    response_time_warning  = 3000
-    response_time_critical = 5000
-    response_time_duration = 3
-    max_check_attempts     = 3
-    headers                = {
-      Cache-Control = "no-cache"
-      Authorization = "Basic ${base64encode(format("%s:%s", data.onepassword_item.lily-epgstation_credential.username, data.onepassword_item.lily-epgstation_credential.password))}"
-    }
-    contains_string                   = "{"
-    certification_expiration_warning  = 7
-    certification_expiration_critical = 3
-    follow_redirect                   = false
-  }
-
-  depends_on = [data.onepassword_item.lily-epgstation_credential]
-}
--- terraform/modules/starry-blue/cloudflare_record_cname_mahiron_api_v6.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname_mahiron_api_v6.tf
@@ -5,31 +5,3 @@ resource "cloudflare_record" "cname_mahiron_api_v6" {
   type    = "CNAME"
   proxied = false
 }
-
-resource "mackerel_service" "mahiron_api_v6" {
-  name = "Lily_Mahiron-API-IPv6"
-}
-
-resource "mackerel_monitor" "mahiron_api_v6" {
-  name = format("%s に疎通できない", cloudflare_record.cname_mahiron_api_v6.hostname)
-
-  external {
-    method                 = "GET"
-    url                    = format("https://%s/api/status", cloudflare_record.cname_mahiron_api_v6.hostname)
-    service                = mackerel_service.mahiron_api_v6.name
-    response_time_warning  = 3000
-    response_time_critical = 5000
-    response_time_duration = 3
-    max_check_attempts     = 3
-    headers                = {
-      Cache-Control = "no-cache"
-      Authorization = "Basic ${base64encode(format("%s:%s", data.onepassword_item.lily-mahiron_credential.username, data.onepassword_item.lily-mahiron_credential.password))}"
-    }
-    contains_string                   = "{"
-    certification_expiration_warning  = 7
-    certification_expiration_critical = 3
-    follow_redirect                   = false
-  }
-
-  depends_on = [data.onepassword_item.lily-mahiron_credential]
-}
