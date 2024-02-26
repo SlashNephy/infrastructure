6240dfa10939537658cd33f9487d2d51e811f94e
SlashNephy <spica@starry.blue>
2024-02-26 13:40:20 +0900

    *-api-v6.starry.blue は使われていない

--- terraform/modules/starry-blue/cloudflare_record_cname_mirakurun_api_v6.tf
+++
@@ -1,7 +0,0 @@
-resource "cloudflare_record" "cname_mirakurun_api_v6" {
-  zone_id = cloudflare_zone.zone.id
-  name    = "mirakurun-api-v6"
-  value   = cloudflare_record.aaaa_gateway_v6.hostname
-  type    = "CNAME"
-  proxied = false
-}
