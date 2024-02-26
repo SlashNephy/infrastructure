d65ddee05bc7b21aab2cc15c199eba43554d808b
SlashNephy <spica@starry.blue>
2024-02-26 14:42:07 +0900

    gateway.starry.blue -> gateway-v4.starry.blue

--- terraform/modules/starry-blue/cloudflare_record_a_gateway_v4.tf
+++ terraform/modules/starry-blue/cloudflare_record_a_gateway_v4.tf
@@ -1,4 +1,4 @@
-resource "cloudflare_record" "a_gateway" {
+resource "cloudflare_record" "a_gateway_v4" {
   zone_id         = cloudflare_zone.zone.id
   name            = "gateway-v4"
   type            = "A"
--- terraform/modules/starry-blue/cloudflare_record_cname_epgstation_api.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname_epgstation_api.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "cname_epgstation_api" {
   zone_id = cloudflare_zone.zone.id
   name    = "epgstation-api"
-  value   = cloudflare_record.a_gateway.hostname
+  value   = cloudflare_record.a_gateway_v4.hostname
   type    = "CNAME"
   proxied = false
 }
