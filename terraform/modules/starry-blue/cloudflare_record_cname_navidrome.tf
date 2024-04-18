32e81cdeaeb2963f2b39c4e0a2561f831a668c03
SlashNephy <spica@starry.blue>
2024-04-19 00:43:58 +0900

    Navidrome

---
+++ terraform/modules/starry-blue/cloudflare_record_cname_navidrome.tf
@@ -0,0 +1,35 @@
+resource "cloudflare_record" "cname_navidrome" {
+  zone_id = cloudflare_zone.zone.id
+  name    = "navidrome"
+  value   = cloudflare_record.aaaa_gateway_v6.hostname
+  type    = "CNAME"
+  proxied = true
+}
+
+resource "mackerel_service" "navidrome" {
+  name = "Lily_Navidrome"
+}
+
+resource "mackerel_monitor" "navidrome" {
+  name = format("%s に疎通できない", cloudflare_record.cname_navidrome.hostname)
+
+  external {
+    method                 = "GET"
+    url                    = format("https://%s", cloudflare_record.cname_navidrome.hostname)
+    service                = mackerel_service.navidrome.name
+    response_time_warning  = 3000
+    response_time_critical = 5000
+    response_time_duration = 3
+    max_check_attempts     = 1
+    headers = {
+      Cache-Control           = "no-cache"
+      CF-Access-Client-Id     = data.onepassword_item.cloudflare_access_client.username
+      CF-Access-Client-Secret = data.onepassword_item.cloudflare_access_client.password
+    }
+    certification_expiration_warning  = 7
+    certification_expiration_critical = 3
+    follow_redirect                   = false
+  }
+
+  depends_on = [data.onepassword_item.cloudflare_access_client]
+}
