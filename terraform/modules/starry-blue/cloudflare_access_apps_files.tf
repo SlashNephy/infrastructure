89228cdd834e40b3ee7d9d6e0af7c01676d210aa
SlashNephy <spica@starry.blue>
2024-04-20 04:53:04 +0900

    disable files

--- terraform/modules/starry-blue/cloudflare_access_apps_files.tf
+++ terraform/modules/starry-blue/cloudflare_access_apps_files.tf
@@ -1,8 +1,8 @@
-resource "cloudflare_access_application" "files" {
-  account_id           = cloudflare_account.account.id
-  name                 = cloudflare_record.cname_files.hostname
-  domain               = cloudflare_record.cname_files.hostname
-  type                 = "bookmark"
-  logo_url             = "https://github.com/filebrowser/logo/raw/master/icon.svg"
-  app_launcher_visible = true
-}
+# resource "cloudflare_access_application" "files" {
+#   account_id           = cloudflare_account.account.id
+#   name                 = cloudflare_record.cname_files.hostname
+#   domain               = cloudflare_record.cname_files.hostname
+#   type                 = "bookmark"
+#   logo_url             = "https://github.com/filebrowser/logo/raw/master/icon.svg"
+#   app_launcher_visible = true
+# }
