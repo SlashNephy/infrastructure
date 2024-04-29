a7bd7c99225c4f8432d66906c51778d13fb926fd
SlashNephy <spica@starry.blue>
2024-04-29 14:20:32 +0900

    Delete terraform/modules/starry-blue/cloudflare_record_a_gateway_v4.tf
    Signed-off-by: SlashNephy <spica@starry.blue>

--- terraform/modules/starry-blue/cloudflare_record_a_gateway_v4.tf
+++
@@ -1,7 +0,0 @@
-resource "cloudflare_record" "a_gateway_v4" {
-  zone_id         = cloudflare_zone.zone.id
-  name            = "gateway-v4"
-  type            = "A"
-  proxied         = false
-  allow_overwrite = true
-}
