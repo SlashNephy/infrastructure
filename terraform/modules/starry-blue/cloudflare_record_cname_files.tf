702dea4e99287c49d0447c28369d2845ec44c436
SlashNephy <spica@starry.blue>
2024-05-05 01:54:44 +0900

    files.gateway

--- terraform/modules/starry-blue/cloudflare_record_cname_files.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname_files.tf
@@ -29,3 +29,11 @@ resource "mackerel_monitor" "files" {
     follow_redirect                   = false
   }
 }
+
+resource "cloudflare_record" "cname_files_gateway" {
+  zone_id = cloudflare_zone.zone.id
+  name    = "files.gateway"
+  value   = cloudflare_record.aaaa_gateway_v6.hostname
+  type    = "CNAME"
+  proxied = false
+}
