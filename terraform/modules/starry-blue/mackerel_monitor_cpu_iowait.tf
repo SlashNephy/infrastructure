212d9e7c7dcae850b6653da3b26f3b0f52f1012f
SlashNephy <slashnephy@hatena.ne.jp>
2024-05-05 20:54:46 +0900

    iowait の閾値を調整する

--- terraform/modules/starry-blue/mackerel_monitor_cpu_iowait.tf
+++ terraform/modules/starry-blue/mackerel_monitor_cpu_iowait.tf
@@ -4,8 +4,8 @@ resource "mackerel_monitor" "cpu_iowait" {
   host_metric {
     metric             = "cpu.iowait.percentage"
     operator           = ">"
-    warning            = "400"
-    critical           = "500"
+    warning            = "300"
+    critical           = "400"
     duration           = 5
     max_check_attempts = 3
     scopes             = [mackerel_role.server.id]
