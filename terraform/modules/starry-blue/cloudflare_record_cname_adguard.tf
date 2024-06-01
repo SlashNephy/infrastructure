6ca01d20099ce76a9179360c0eb439565983aba6
SlashNephy <spica@starry.blue>
2024-06-02 03:25:19 +0900

    AdGuard Home

---
+++ terraform/modules/starry-blue/cloudflare_record_cname_adguard.tf
@@ -0,0 +1,28 @@
+resource "cloudflare_record" "cname_adguard" {
+  zone_id = cloudflare_zone.zone.id
+  name    = "adguard"
+  value   = cloudflare_record.aaaa_gateway_v6.hostname
+  type    = "CNAME"
+  proxied = true
+}
+
+resource "mackerel_service" "adguard" {
+  name = "Lily_AdGuard-Home"
+}
+
+resource "mackerel_monitor" "adguard" {
+  name = format("%s に疎通できない", cloudflare_record.cname_adguard.hostname)
+
+  external {
+    method                            = "GET"
+    url                               = format("https://%s/healthcheck", cloudflare_record.cname_adguard.hostname)
+    service                           = mackerel_service.adguard.name
+    response_time_warning             = 3000
+    response_time_critical            = 5000
+    response_time_duration            = 3
+    max_check_attempts                = 1
+    certification_expiration_warning  = 7
+    certification_expiration_critical = 3
+    follow_redirect                   = false
+  }
+}
