d9c41efec21a505706f83b1d3b83c0f040419d5c
SlashNephy <spica@starry.blue>
2024-06-03 13:51:33 +0900

    proxied = false

--- terraform/modules/starry-blue/cloudflare_record_cname_zero_ssl.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname_zero_ssl.tf
@@ -3,5 +3,5 @@ resource "cloudflare_record" "cname_zero_ssl_verify" {
   name    = "_3335B0FD3B631E8CFB6F3508E480BD09.dns"
   value   = "FC30F1F227F8B562E855021BDFF33CC4.30E939B8461358691AFF4FDFC423A6F3.c2bd6d22c368d07.comodoca.com"
   type    = "CNAME"
-  proxied = true
+  proxied = false
 }
