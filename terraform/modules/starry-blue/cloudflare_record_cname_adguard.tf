7f330c213333b79118cd43af30a39318d22ada05
SlashNephy <spica@starry.blue>
2024-07-15 13:53:05 +0900

    もう不要

--- terraform/modules/starry-blue/cloudflare_record_cname_adguard.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname_adguard.tf
@@ -5,24 +5,3 @@ resource "cloudflare_record" "cname_dns" {
   type    = "CNAME"
   proxied = false
 }
-
-resource "mackerel_service" "dns" {
-  name = "Lily_AdGuard-Home"
-}
-
-resource "mackerel_monitor" "dns" {
-  name = format("%s に疎通できない", cloudflare_record.cname_dns.hostname)
-
-  external {
-    method                            = "GET"
-    url                               = format("https://%s/healthcheck", cloudflare_record.cname_dns.hostname)
-    service                           = mackerel_service.dns.name
-    response_time_warning             = 3000
-    response_time_critical            = 5000
-    response_time_duration            = 3
-    max_check_attempts                = 1
-    certification_expiration_warning  = 7
-    certification_expiration_critical = 3
-    follow_redirect                   = false
-  }
-}
