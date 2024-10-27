88d9e5179f4b67121cb62b68caccde9985c6bb42
SlashNephy <spica@starry.blue>
2024-10-27 12:03:42 +0900

    不要リソースを整理

--- terraform/modules/starry-blue/cloudflare_record_cname_adguard.tf
+++
@@ -1,7 +0,0 @@
-resource "cloudflare_record" "cname_dns" {
-  zone_id = cloudflare_zone.zone.id
-  name    = "dns"
-  content = cloudflare_record.aaaa_gateway_v6.hostname
-  type    = "CNAME"
-  proxied = false
-}
