9c6425cb3eedd6b0b8b01b0da4f94bc030dc916d
SlashNephy <spica@starry.blue>
2024-11-26 23:55:11 +0900

    もうええでしょう

--- terraform/modules/starry-blue/cloudflare_access_group_everyone.tf
+++
@@ -1,8 +0,0 @@
-resource "cloudflare_access_group" "everyone" {
-  account_id = cloudflare_account.account.id
-  name       = "Everyone"
-
-  include {
-    everyone = true
-  }
-}
--- terraform/modules/starry-blue/secrets.tf
+++ terraform/modules/starry-blue/secrets.tf
@@ -2,11 +2,6 @@ data "onepassword_vault" "terraform" {
   uuid = "7y5v3rmq4dpwnk2bgbm7vig3su"
 }
 
-data "onepassword_item" "github_oauth_client" {
-  vault = data.onepassword_vault.terraform.uuid
-  uuid  = "47j2n5ecdeulpja5mh5hud7eom"
-}
-
 data "onepassword_item" "general_slack_webhook_url" {
   vault = data.onepassword_vault.terraform.uuid
   uuid  = "y7x5qzuvf4bkqw6g7uosnqwfw4"
