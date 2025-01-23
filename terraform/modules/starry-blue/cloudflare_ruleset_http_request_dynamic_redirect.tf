4cce16a4bf1847e8c95eb464d8d49d6ed6d17b40
SlashNephy <spica@starry.blue>
2025-01-23 13:11:46 +0900

    まだあった

--- terraform/modules/starry-blue/cloudflare_ruleset_http_request_dynamic_redirect.tf
+++ terraform/modules/starry-blue/cloudflare_ruleset_http_request_dynamic_redirect.tf
@@ -22,23 +22,4 @@ resource "cloudflare_ruleset" "http_request_dynamic_redirect" {
       (http.host eq "${cloudflare_record.cname_www.hostname}")
     EOT
   }
-
-  rules {
-    enabled     = true
-    description = "${cloudflare_record.cname_root.hostname} -> ${cloudflare_record.cname_spica.hostname}"
-    action      = "redirect"
-    action_parameters {
-      from_value {
-        status_code = 302
-        target_url {
-          value = "https://${cloudflare_record.cname_spica.hostname}"
-        }
-        preserve_query_string = false
-      }
-    }
-
-    expression = <<-EOT
-      (http.host eq "${cloudflare_record.cname_root.hostname}")
-    EOT
-  }
 }
