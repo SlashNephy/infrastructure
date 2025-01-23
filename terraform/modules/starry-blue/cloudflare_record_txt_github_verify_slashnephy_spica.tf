9379d3faa71f64b7229b47b7ee034897ff67ec3d
SlashNephy <spica@starry.blue>
2025-01-23 13:06:17 +0900

    Terraform の管理から外す

--- terraform/modules/starry-blue/cloudflare_record_txt_github_verify_slashnephy_spica.tf
+++
@@ -1,6 +0,0 @@
-resource "cloudflare_record" "txt_github_verify_slashnephy_spica" {
-  zone_id = cloudflare_zone.zone.id
-  name    = "_github-pages-challenge-slashnephy.spica"
-  content = "14400f193e1665a52e3788180c7230"
-  type    = "TXT"
-}
