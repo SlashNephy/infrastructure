c00a842cb01f97f006db7001689dd594d959b161
SlashNephy <spica@starry.blue>
2025-01-24 17:59:43 +0900

    まだあった

--- terraform/modules/starry-blue/cloudflare_record_cname_cydia.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname_cydia.tf
@@ -5,24 +5,3 @@ resource "cloudflare_record" "cname_cydia" {
   type    = "CNAME"
   proxied = true
 }
-
-resource "mackerel_service" "cydia" {
-  name = "cydia-starry-blue"
-}
-
-resource "mackerel_monitor" "cydia" {
-  name = cloudflare_record.cname_cydia.hostname
-
-  external {
-    method                            = "GET"
-    url                               = format("https://%s", cloudflare_record.cname_cydia.hostname)
-    service                           = mackerel_service.cydia.name
-    response_time_warning             = 5000
-    response_time_critical            = 10000
-    response_time_duration            = 3
-    max_check_attempts                = 1
-    certification_expiration_warning  = 30
-    certification_expiration_critical = 7
-    follow_redirect                   = false
-  }
-}
