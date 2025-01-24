b8c2ff2c87f61dc8bdbbc30e35ea41e4d8bfbb24
SlashNephy <spica@starry.blue>
2025-01-24 17:57:24 +0900

    不要ルールを整理

--- terraform/modules/starry-blue/cloudflare_record_cname_www.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname_www.tf
@@ -5,24 +5,3 @@ resource "cloudflare_record" "cname_www" {
   type    = "CNAME"
   proxied = true
 }
-
-resource "mackerel_service" "www" {
-  name = "www-starry-blue"
-}
-
-resource "mackerel_monitor" "www" {
-  name = cloudflare_record.cname_www.hostname
-
-  external {
-    method                            = "GET"
-    url                               = format("https://%s", cloudflare_record.cname_www.hostname)
-    service                           = mackerel_service.www.name
-    response_time_warning             = 5000
-    response_time_critical            = 10000
-    response_time_duration            = 3
-    max_check_attempts                = 1
-    certification_expiration_warning  = 30
-    certification_expiration_critical = 7
-    follow_redirect                   = false
-  }
-}
