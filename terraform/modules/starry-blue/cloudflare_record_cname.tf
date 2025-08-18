2dab473652f8f124884b1c418334d092876a6d73
SlashNephy <spica@starry.blue>
2025-08-19 00:14:12 +0900

    ワイルドカードの CNAME を追加

--- terraform/modules/starry-blue/cloudflare_record_cname.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname.tf
@@ -5,3 +5,19 @@ resource "cloudflare_record" "cname_root" {
   type    = "CNAME"
   proxied = true
 }
+
+resource "cloudflare_record" "cname_wildcard" {
+  zone_id = cloudflare_zone.zone.id
+  name    = "*"
+  content = data.cloudflare_record.aaaa_gateway_v6.hostname
+  type    = "CNAME"
+  proxied = true
+}
+
+resource "cloudflare_record" "cname_wildcard_gateway" {
+  zone_id = cloudflare_zone.zone.id
+  name    = "*.gateway"
+  content = data.cloudflare_record.aaaa_gateway_v6.hostname
+  type    = "CNAME"
+  proxied = false
+}
