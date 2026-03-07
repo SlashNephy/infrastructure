418fb5b0da32a393d48d5b8c3af6f61488246a1f
SlashNephy <slashnephy@hatena.ne.jp>
2026-03-07 21:53:09 +0900

    openclaw

---
+++ terraform/modules/starry-blue/cloudflare_record_cname_openclaw.tf
@@ -0,0 +1,29 @@
+resource "cloudflare_record" "cname_openclaw" {
+  zone_id = cloudflare_zone.zone.id
+  name    = "openclaw"
+  content = data.cloudflare_record.aaaa_gateway_v6.hostname
+  type    = "CNAME"
+  proxied = true
+}
+
+resource "mackerel_service" "openclaw" {
+  name = "Lily_openclaw"
+}
+
+resource "mackerel_monitor" "openclaw" {
+  name = cloudflare_record.cname_openclaw.hostname
+
+  external {
+    method                            = "GET"
+    url                               = format("https://%s", cloudflare_record.cname_openclaw.hostname)
+    expected_status_code              = 302
+    service                           = mackerel_service.openclaw.name
+    response_time_warning             = 5000
+    response_time_critical            = 10000
+    response_time_duration            = 5
+    max_check_attempts                = 1
+    certification_expiration_warning  = 30
+    certification_expiration_critical = 7
+    follow_redirect                   = false
+  }
+}
