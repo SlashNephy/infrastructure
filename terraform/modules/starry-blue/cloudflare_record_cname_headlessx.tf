7e71371d473fb7a8f5bb932f40205c3d6db7283c
SlashNephy <slashnephy@hatena.ne.jp>
2026-03-15 00:45:14 +0900

    api は使わないので閉じる

--- terraform/modules/starry-blue/cloudflare_record_cname_headlessx.tf
+++ terraform/modules/starry-blue/cloudflare_record_cname_headlessx.tf
@@ -5,11 +5,3 @@ resource "cloudflare_record" "cname_headlessx" {
   type    = "CNAME"
   proxied = true
 }
-
-resource "cloudflare_record" "cname_headlessx_api" {
-  zone_id = cloudflare_zone.zone.id
-  name    = "headlessx-api"
-  content = data.cloudflare_record.aaaa_gateway_v6.hostname
-  type    = "CNAME"
-  proxied = true
-}
